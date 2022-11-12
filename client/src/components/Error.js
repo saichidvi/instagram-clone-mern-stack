c4ac43d97c7b88a09665df39d03bc5d2108cd29f
saichidvi <saichidvigupta@gmail.com>
2022-11-12 11:45:42 +0530

    made the errorPage

---
+++ client/src/components/Error.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Error.css";
+
+const Error = () => {
+    return(
+        <div>
+            Hello This is Error Page
+        </div>
+    );
+};
+
+
+export default Error;
